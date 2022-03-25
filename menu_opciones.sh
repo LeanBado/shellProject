992f39dbf87800036dd7ce5a74edb8f6222830bf
LeanBado <leandro.bado.42@gmail.com>
2022-03-25 20:10:51 -0300

    menu de instalacion Postgres 2/2

--- menu_opciones.sh
+++ menu_opciones.sh
@@ -1,7 +1,8 @@
-#! /bin/bash
+#! /bin/bashi
 #Menú de opciones
 
 option=0
+fechaActual=`date +%d%m%Y`
 
                             #funcion de instalacion de Postgres
 instalar_postgres () {
@@ -43,14 +44,49 @@ desinstalar_postgres () {
 
                             #funcion para generar respaldo
 generar_respaldo () {
-    echo -e "\nGenerando respaldo..."
-    echo "Directorio del backup: "$1
+  echo "Listar las bases de datos"
+  sudo -u postgres psql -c "\l"
+  read -p "Elegir la base de datos a respaldar:" bddRespaldo
+  echo -e "\n"
+      if [ -d "$1" ]; then
+          echo "Establecer permisos directorio"
+          echo "$password" | sudo -S chmod 755 $1
+          echo "Realizando respaldo..."
+          sudo -u postgres pg_dump -Fc $bddRespaldo > "$1/bddRespaldo$fechaActual.bak"
+          echo "Respaldo realizado correctamente en la ubicaciÃ³n:$1/bddRespaldo$fechaActual.bak"
+      else
+          echo "El directorio $1 no existe"
+      fi
+read -n1 -s -r -p "Para continuar presione cualquier tecla..."
 }
+
                             #funcion para restaurar el respaldo generado
 restaurar_respaldo () {
-    echo -e "\nRestaurando respaldo..."
-    echo "Directorio del respaldo: " $1
+    echo "Listar respaldos"
+    ls -1 $1/*.bak
+    read -p "Elegir el respaldo a restaurar:" respaldoRestaurar
+    echo -e "\n"
+    read -p "Ingrese el nombre de la base de datos destino:" bddDestino
+                            #Verificar si la bdd existe
+    verifyBdd=$(sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -wq $bddDestino)
+    if [ $? -eq 0 ]; then
+        echo "Restaurando en la bdd destino: $bddDestino"
+    else
+        sudo -u postgres psql -c "create database $bddDestino"
+    fi
+
+    if [ -f "$1/$respaldoRestaurar" ]; then
+        echo "Restaurando respaldo..."
+        sudo -u postgres pg_restore -Fc -d $bddDestino "$1/$respaldoRestaurar"
+        echo "Listar la base de datos"
+        sudo -u postgres psql -c "\l"
+    else
+        echo "El respaldo $respaldoRestaurar no existe"
+    fi    
+
+read -n1 -s -r -p "Para continuar presione cualquier tecla..."
 }
+
 
 while :
     do
